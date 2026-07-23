634e5a5f8e6ee33bafe656dec9dd117103d84399
hashirsohail <hashirsohail18@gmail.com>
2026-07-24 04:30:12 +0500

    Yet Another Two Integers Problem

---
+++ 800/1409A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int a, b; cin >> a >> b;
+    int d = abs(a-b);
+    cout << (d+9)/10 << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tt; 
+    cin >> tt;
+    while (tt--) {
+        solve();
+    }
+    
+    return 0;
+}
