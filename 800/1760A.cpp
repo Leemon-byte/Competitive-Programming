9c103f387bbfbf745464e53f734534906d8d3b5e
hashirsohail <hashirsohail18@gmail.com>
2026-07-24 04:38:37 +0500

    Medium Number

---
+++ 800/1760A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    vector<int> v(3);
+    for(int i=0; i<3;i++)
+    cin >> v[i];
+    sort(v.begin(), v.end());
+    cout << v[1] << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tt; 
+    cin >> tt;
+    while (tt--) {
+        solve();
+    }
+    
+    return 0;
+}
