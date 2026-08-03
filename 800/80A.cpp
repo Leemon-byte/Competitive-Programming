5d0bc3e9a3438ea86f5fddb585d8e4a3e92ba0e9
hashirsohail <hashirsohail18@gmail.com>
2026-08-04 01:06:13 +0500

    Comment

--- 800/80A.cpp
+++ 800/80A.cpp
@@ -9,7 +9,7 @@ bool isPrime(long long n){
     if(n % 2 == 0)
     return false;
 
-    for(int i=3; i*i <= n; i++){
+    for(int i=3; i*i <= n; i+=2){ // skips even numbers
         if(n % i == 0)
         return false;
     }
