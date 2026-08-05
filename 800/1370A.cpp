dcaffcf39de670376161e0793e1fc443c46ed980
hashirsohail <hashirsohail18@gmail.com>
2026-08-05 22:44:04 +0500

    Maximum GCD

---
+++ 800/1370A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n; cin >> n;
+    cout << n/2 << '\n';
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int tt; cin >> tt;
+    while(tt--){
+        solve();
+    }
+
+    return 0;
+}
