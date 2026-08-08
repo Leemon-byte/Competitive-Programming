c291ac283a165e97dd1d3e3722f1300cd9b70bbe
hashirsohail <hashirsohail18@gmail.com>
2026-08-08 21:19:28 +0500

    Boring Apartment

---
+++ 800/1433A.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int x; cin >> x;
+    // convert into string 
+    string s = to_string(x);
+
+    int p = s[0] - '0'; // p * the sum
+    p--; 
+    int ans = 0;
+
+    if(s.length() == 4)
+    ans = (p*10) + 10;
+    if(s.length() == 3)
+    ans = (p*10) + 6;
+    if(s.length() == 2)
+    ans = (p*10) + 3;
+    if(s.length() == 1)
+    ans = (p*10) + 1;
+
+    cout << ans << "\n";
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int tt; cin >> tt;
+    while(tt--){
+        solve();
+    }
+
+    return 0;
+}
