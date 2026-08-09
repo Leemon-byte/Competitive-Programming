b877d8966be15ee15cbc01ab758e256675f4d2f4
hashirsohail <hashirsohail18@gmail.com>
2026-08-09 05:23:39 +0500

    Jagged Swaps

---
+++ 800/1896A.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n; cin >> n;
+    vector<int> v(n);
+    for(int i=0; i<n;i++)
+    cin >> v[i];
+    for(int i=0; i<n;i++){
+        for(int i=1; i<n-1;i++){
+            if(v[i-1] < v[i] && v[i] > v[i+1]){
+                // swap the values
+                int temp = v[i];
+                v[i] = v[i+1];
+                v[i+1] = temp;
+            }
+        }
+    }
+    // if sorted then yes else no
+    bool flag = true;
+    for(int i=0; i<n-1;i++){
+        if(v[i] > v[i+1]) // means not sorted
+        flag = false;
+    }
+    if(flag)
+    cout << "YES\n";
+    else 
+    cout << "NO\n";
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int tt; cin >> tt;
+    while(tt--){
+        solve();
+    }
+
+    return 0;
+}
